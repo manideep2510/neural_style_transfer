267329bebc75cbd3f091d59e76fae54aa90eb1dd
Manideep <manideep2510@gmail.com>
2018-07-11 13:24:05 +0530

    images update

--- resize.py
+++ resize.py
@@ -6,21 +6,20 @@ from scipy.misc import imsave
 import matplotlib.pyplot as plt
 from PIL import Image
 
-imgsx1 = plt.imread("images/stary_night.jpg")
+imgsx1 = plt.imread("images/style_night.jpg")
 imgsx1_resize = imresize(imgsx1, (512, 512))
 
 imx1 = Image.fromarray(imgsx1_resize)
-imx1.save("images/stary_night1.jpg")
+imx1.save("images/style_night1.jpg")
 
-imgsx2 = plt.imread("images/Red_vineyards.jpg")
+imgsx2 = plt.imread("images/manideep.jpg")
 imgsx2_resize = imresize(imgsx2, (512, 512))
 
 imx2 = Image.fromarray(imgsx2_resize)
-imx2.save("images/Red_vineyards1.jpg")
+imx2.save("images/manideep1.jpg")
 
-imgsx2 = plt.imread("images/irises.jpg")
+imgsx2 = plt.imread("images/style1.jpg")
 imgsx2_resize = imresize(imgsx2, (512, 512))
 
 imx2 = Image.fromarray(imgsx2_resize)
-imx2.save("images/irises1.jpg")
-
+imx2.save("images/style_11.jpg")
